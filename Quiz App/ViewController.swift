cf255aa716bf60b2c1919d9d8a90d636e6ae699f
Kraig Larson <Kraigalarson@gmail.com>
2023-12-05 12:46:40 -0500

    Initial Commit

---
+++ Quiz App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quiz App
+//
+//  Created by Krajg Larson on 12/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
